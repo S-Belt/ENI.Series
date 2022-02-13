a41e30545f9b3517ddaeb6b8a4f114fee6d8f4c3
S-Belt <beltadzes@gmail.com>
2022-02-13 20:06:12 +0100

    utilisation de twig dans templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,8 +13,7 @@ class MainController extends AbstractController
 
     public function home()
     {
-     echo "coucou";
-     die();
+     return $this->render('main/home.html.twig');
     }
 
     /**
@@ -23,8 +22,7 @@ class MainController extends AbstractController
 
     public function test()
     {
-        echo "test, test, allo!";
-        die();
+        return $this->render('main/test.html.twig');
     }
 
 }
